e6290df70ae1b366fd59864b51ac0e65b948e24b
m1xxos <66390094+m1xxos@users.noreply.github.com>
2024-04-17 22:36:37 +0300

    feat:cloudflare tunnel test

--- terraform/proxy.tf
+++ terraform/proxy.tf
@@ -26,3 +26,14 @@ resource "nginxproxymanager_proxy_host" "k3s_proxy" {
   ssl_forced              = true
   certificate_id          = 3
 }
+
+resource "nginxproxymanager_proxy_host" "tunnel_proxmox_proxy" {
+  domain_names            = ["proxmox.m1xxos.me"]
+  forward_host            = "192.168.1.122"
+  forward_port            = 8006
+  forward_scheme          = "https"
+  allow_websocket_upgrade = true
+  block_exploits          = true
+  ssl_forced              = true
+  certificate_id          = 4
+}
