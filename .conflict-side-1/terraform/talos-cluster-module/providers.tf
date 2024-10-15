a32996356517e0c2900de4395c8a18b1c4d72e2f
m1xxos <66390094+m1xxos@users.noreply.github.com>
2024-10-15 23:26:31 +0300

    feat:helm install cilium

--- .conflict-side-1/terraform/talos-cluster-module/providers.tf
+++ .conflict-side-1/terraform/talos-cluster-module/providers.tf
@@ -12,5 +12,23 @@ terraform {
       source  = "siderolabs/talos"
       version = "0.5.0"
     }
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.16.0"
+    }
+  }
+}
+
+locals {
+    kube_config = yamldecode(data.talos_cluster_kubeconfig.kubeconfig.kubeconfig_raw)
+}
+
+provider "helm" {
+  kubernetes {
+    host = local.kube_config.clusters[0].cluster.server
+    cluster_ca_certificate = base64decode(local.kube_config.clusters[0].cluster.certificate-authority-data)
+
+    client_certificate = base64decode(local.kube_config.users[0].user.client-certificate-data)
+    client_key = base64decode(local.kube_config.users[0].user.client-key-data)
   }
 }
