907357f5eb454caf2740054abaf4255b5e5a7bd5
m1xxos <66390094+m1xxos@users.noreply.github.com>
2024-10-15 22:26:48 +0300

    feat:proxmox file

---
+++ .conflict-base-0/terraform/proxy.tf
@@ -0,0 +1,111 @@
+provider "nginxproxymanager" {
+  host     = "http://192.168.1.250:81"
+  username = var.nginx_username
+  password = var.nginx_password
+}
+
+
+resource "nginxproxymanager_proxy_host" "proxmox_proxy" {
+  domain_names            = ["proxmox.local.m1xxos.me"]
+  forward_host            = "192.168.1.122"
+  forward_port            = 8006
+  forward_scheme          = "https"
+  allow_websocket_upgrade = true
+  block_exploits          = true
+  ssl_forced              = true
+  http2_support           = true
+  hsts_enabled            = true
+  certificate_id          = 1
+}
+
+resource "nginxproxymanager_proxy_host" "k3s_proxy" {
+  domain_names            = ["*.local.m1xxos.me"]
+  forward_host            = "192.168.1.100"
+  forward_port            = 443
+  forward_scheme          = "https"
+  allow_websocket_upgrade = true
+  block_exploits          = true
+  ssl_forced              = true
+  http2_support           = true
+  hsts_enabled            = true
+  certificate_id          = 1
+}
+
+resource "nginxproxymanager_proxy_host" "dev_kuber_proxy" {
+  domain_names            = ["*.dev.m1xxos.me"]
+  forward_host            = "192.168.1.115"
+  forward_port            = 80
+  forward_scheme          = "http"
+  allow_websocket_upgrade = true
+  block_exploits          = true
+  # ssl_forced              = true
+  # http2_support           = true
+  # hsts_enabled            = true
+  # certificate_id          = 4
+
+}
+
+resource "nginxproxymanager_proxy_host" "tunnel_proxmox_proxy" {
+  domain_names            = ["proxmox.m1xxos.me"]
+  forward_host            = "192.168.1.122"
+  forward_port            = 8006
+  forward_scheme          = "https"
+  allow_websocket_upgrade = true
+  block_exploits          = true
+  ssl_forced              = true
+  http2_support           = true
+  hsts_enabled            = true
+  certificate_id          = 3
+}
+
+resource "nginxproxymanager_proxy_host" "portainer_proxy" {
+  domain_names            = ["portainer.local.m1xxos.me"]
+  forward_host            = "192.168.1.99"
+  forward_port            = 9443
+  forward_scheme          = "https"
+  allow_websocket_upgrade = true
+  block_exploits          = true
+  ssl_forced              = true
+  http2_support           = true
+  hsts_enabled            = true
+  certificate_id          = 1
+}
+
+resource "nginxproxymanager_proxy_host" "gitlab_proxy" {
+  domain_names            = ["gitlab.local.m1xxos.me"]
+  forward_host            = "192.168.1.228"
+  forward_port            = 443
+  forward_scheme          = "https"
+  allow_websocket_upgrade = true
+  block_exploits          = true
+  ssl_forced              = true
+  http2_support           = true
+  hsts_enabled            = true
+  certificate_id          = 1
+}
+
+resource "nginxproxymanager_proxy_host" "registry_proxy" {
+  domain_names            = ["registry.local.m1xxos.me"]
+  forward_host            = "192.168.1.228"
+  forward_port            = 443
+  forward_scheme          = "https"
+  allow_websocket_upgrade = true
+  block_exploits          = true
+  ssl_forced              = true
+  http2_support           = true
+  hsts_enabled            = true
+  certificate_id          = 1
+}
+
+resource "nginxproxymanager_proxy_host" "home_proxy" {
+  domain_names            = ["*.home.m1xxos.me", "home.m1xxos.me"]
+  forward_host            = "192.168.1.99"
+  forward_port            = 443
+  forward_scheme          = "https"
+  allow_websocket_upgrade = true
+  block_exploits          = true
+  ssl_forced              = true
+  http2_support           = true
+  hsts_enabled            = true
+  certificate_id          = 11
+}
---
+++ .conflict-side-1/terraform/variable.tf
@@ -0,0 +1,61 @@
+variable "proxmox_api_url" {
+  type = string
+}
+
+variable "proxmox_api_token_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "proxmox_api_token_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "proxmox_talos_api_token" {
+  type = string
+  sensitive = true
+  
+}
+
+variable "ssh_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "bucket" {
+  type = string
+}
+
+variable "access_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "secret_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "cloudflare_api_token" {
+  type      = string
+  sensitive = true
+}
+
+variable "cloudflare_zone_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "nginx_username" {
+  type = string
+}
+
+variable "nginx_password" {
+  type = string
+}
+
+variable "authentik_token" {
+  type      = string
+  sensitive = true
+}
