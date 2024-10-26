cc4dd881e27bfbaf4ce5d8af7f089fce8c5a28d1
m1xxos <66390094+m1xxos@users.noreply.github.com>
2024-10-26 23:11:30 +0300

    feat:dashboard test

--- terraform/main.tf
+++ terraform/main.tf
@@ -157,6 +157,9 @@ module "test-cluster" {
       ip    = "192.168.1.74"
     }
   ]
+
+  external_ip        = "192.168.1.80"
+  worker_disk_size   = 40
   cloudflare_zone_id = var.cloudflare_zone_id
   cluster_name       = "test-cluster"
   talos_image_id     = proxmox_virtual_environment_download_file.talos_nocloud_image.id
