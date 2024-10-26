f1aa19e7c4768c441fd6a8f1e4b637f1f331da83
GitButler <gitbutler@gitbutler.com>
2024-10-26 22:04:38 +0300

    GitButler Workspace Commit
    This is a merge commit the virtual branches in your workspace.
    
    Due to GitButler managing multiple virtual branches, you cannot switch back and
    forth between git branches and virtual branches easily. 
    
    If you switch to another branch, GitButler will need to be reinitialized.
    If you commit on this branch, GitButler will throw it away.
    
    Here are the branches that are currently applied:
     - 167-big-rebuild (refs/gitbutler/167-big-rebuild)
       branch head: 6ba5d28612b1fcb478e8cc8b4a25a747ca6cb803
    For more information about what we're doing here, check out our docs:
    https://docs.gitbutler.com/features/virtual-branches/integration-branch

--- .conflict-side-1/terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "talosconfig" {
-  value     = module.main-cluster.talosconfig
-  sensitive = true
-}
-
-output "kubeconfig" {
-  value     = module.main-cluster.kubeconfig
-  sensitive = true
-}
--- .conflict-side-1/terraform/talos-cluster-module/flux.tf
+++
@@ -1,6 +0,0 @@
-resource "flux_bootstrap_git" "this" {
-  depends_on = [helm_release.metrics-server]
-
-  embedded_manifests = true
-  path               = "clusters/${var.cluster_name}"
-}
