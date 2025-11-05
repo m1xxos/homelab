492b085db0f1bb5ed2a13166da33c7dd44627677
m1xxos <66390094+m1xxos@users.noreply.github.com>
2025-11-05 16:44:26 +0300

    feat:new talos image

--- terraform/files.tf
+++ terraform/files.tf
@@ -14,3 +14,14 @@ resource "proxmox_virtual_environment_download_file" "talos_nocloud_image" {
   decompression_algorithm = "gz"
   overwrite               = false
 }
+
+resource "proxmox_virtual_environment_download_file" "talos_nocloud_image_1_11_3" {
+  content_type = "iso"
+  datastore_id = "local"
+  node_name    = "plusha"
+
+  file_name               = "talos-v1.11.3-nocloud-amd64-iscsi.img"
+  url                     = "https://factory.talos.dev/image/f2716897efdcc84fb9cef8d04b20631b2ba11de56a941eee5a6577e4a5c08dc7/v1.11.3/nocloud-amd64.raw.gz"
+  decompression_algorithm = "gz"
+  overwrite               = false
+}
