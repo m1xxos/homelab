41685112cb2ec6bc40c230e42b0607dbaea30173
m1xxos <66390094+m1xxos@users.noreply.github.com>
2025-11-09 18:38:10 +0300

    feat:tf restructure

---
+++ terraform/1-vault/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    vault = {
+      source = "hashicorp/vault"
+      version = "5.4.0"
+    }
+  }
+  backend "s3" {
+    endpoint                    = "https://storage.yandexcloud.net"
+    region                      = "ru-central1"
+    bucket                      = var.bucket
+    key                         = "vault-homelab.tfstate"
+    access_key                  = var.access_key
+    secret_key                  = var.secret_key
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+  }
+}
