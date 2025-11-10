0bd918c638d4d8edf2ed5d9bd5d40319828201ad
m1xxos <66390094+m1xxos@users.noreply.github.com>
2025-11-10 19:21:43 +0300

    fix:k8s url

--- terraform/1-vault/auth.tf
+++ terraform/1-vault/auth.tf
@@ -4,7 +4,7 @@ resource "vault_auth_backend" "kubernetes" {
 
 resource "vault_kubernetes_auth_backend_config" "main" {
   backend         = vault_auth_backend.kubernetes.path
-  kubernetes_host = "https://$KUBERNETES_PORT_443_TCP_ADDR:443"
+  kubernetes_host = "https://kubernetes.default.svc:443"
 }
 
 resource "vault_kubernetes_auth_backend_role" "authentik-reader" {
