f37839bd431fc87c9cc03905e4caf9c272e1ebbb
m1xxos <66390094+m1xxos@users.noreply.github.com>
2025-11-10 17:33:09 +0300

    fix:tf policy

--- terraform/1-vault/policy.tf
+++ terraform/1-vault/policy.tf
@@ -1,8 +1,8 @@
 resource "vault_policy" "authentik-reader" {
   name   = var.authentik-sa-name
   policy = <<EOT
-path "main/authentik" {
-  capabilities = ["read", "list"]
+path "main/authentik/*" {
+  capabilities = [ "list", "read" ]
 }
 EOT
 }
