d3686fd86dead154d78b7d3053612a32588c49f9
m1xxos <66390094+m1xxos@users.noreply.github.com>
2025-11-11 16:43:34 +0300

    feat:auth tf project

---
+++ terraform/2-authentik/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    authentik = {
+      source = "goauthentik/authentik"
+      version = "2025.10.0"
+    }
+  }
+  backend "s3" {
+    endpoint                    = "https://storage.yandexcloud.net"
+    region                      = "ru-central1"
+    bucket                      = var.bucket
+    key                         = "authentik-remote-state.tfstate"
+    access_key                  = var.access_key
+    secret_key                  = var.secret_key
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+  }
+}
---
+++ terraform/2-authentik/variables.tf
@@ -0,0 +1,13 @@
+variable "bucket" {
+  type = string
+}
+
+variable "access_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "secret_key" {
+  type      = string
+  sensitive = true
+}
