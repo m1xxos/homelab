d9f097b1ce760785d4e662fa7420e9712e56317d
m1xxos <66390094+m1xxos@users.noreply.github.com>
2025-11-12 13:34:18 +0300

    feat:auth provider

--- terraform/2-authentik/vault.tf
+++ terraform/2-authentik/vault.tf
@@ -10,7 +10,7 @@ resource "random_password" "vault-auth-secret" {
   length = 30
 }
 
-resource "authentik_provider_oauth2" "name" {
+resource "authentik_provider_oauth2" "vault" {
   name               = "vault"
   client_id          = "vault"
   client_secret      = random_password.vault-auth-secret.result
@@ -29,4 +29,10 @@ resource "authentik_provider_oauth2" "name" {
         url = "http://localhost:8250/oidc/callback"
     }
    ]
+}
+
+resource "authentik_application" "vault" {
+  name = "vault"
+  slug = "vault"
+  protocol_provider = authentik_provider_oauth2.vault.id
 }
