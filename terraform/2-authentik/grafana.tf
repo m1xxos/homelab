d4c46a6ec1b3fcf4726504b94b5b205437a0dab8
m1xxos <66390094+m1xxos@users.noreply.github.com>
2025-11-16 18:09:44 +0300

    feat:grafana secrete to vault

--- terraform/2-authentik/grafana.tf
+++ terraform/2-authentik/grafana.tf
@@ -36,3 +36,15 @@ resource "authentik_group" "grafana_editors" {
 resource "authentik_group" "grafana_viewers" {
   name = "Grafana Viewers"
 }
+
+resource "vault_kv_secret_v2" "grafana-auth" {
+  name      = "grafana/grafana-auth"
+  mount     = "main"
+  namespace = authentik_provider_oauth2.vault.client_id
+  data_json = jsonencode(
+    {
+      oidc_client_id     = authentik_provider_oauth2.grafana.client_id
+      oidc_client_secret = authentik_provider_oauth2.grafana.client_secret
+    }
+  )
+}
