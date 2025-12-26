38e73b33dc67c78f06114dcd2d47b8f8004a3f3d
m1xxos <66390094+m1xxos@users.noreply.github.com>
2025-12-26 13:47:54 +0300

    chore:branch update

--- terraform/0-infra/main.tf
+++ terraform/0-infra/main.tf
@@ -45,6 +45,6 @@ module "main-cluster" {
   cluster_name       = "main"
   cluster_dns        = "main.k8s.m1xxos.tech"
   github_token       = var.github_token
-  branch             = "367-api-gateway"
+  branch             = "369-infra-chart-unification"
   cilium_version     = "1.18.4"
 }
