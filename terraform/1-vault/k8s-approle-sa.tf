88ba4e1476eb1ea014225a32c817c92fe6fb729d
m1xxos <66390094+m1xxos@users.noreply.github.com>
2026-01-16 21:57:55 +0300

    feat:approle rid sid auth

--- terraform/1-vault/k8s-approle-sa.tf
+++ terraform/1-vault/k8s-approle-sa.tf
@@ -1,13 +1,17 @@
 resource "vault_auth_backend" "general" {
+  path = "cluster-general"
   type = "approle"
 }
 
 resource "vault_approle_auth_backend_role" "general" {
   backend               = vault_auth_backend.general.path
-  role_name             = "general-k8s"
+  role_name             = "cluster-general-reader"
   token_policies        = ["default", "general-reader"]
-  bind_secret_id        = false
   token_bound_cidrs     = ["192.168.1.0/24"]
   secret_id_bound_cidrs = ["192.168.1.0/24"]
   token_max_ttl         = 600
+}
+
+resource "vault_approle_auth_backend_role_secret_id" "general" {
+  role_name = vault_approle_auth_backend_role.general.role_name
 }
