40732509e0e723ac271f2061afa435b45ebefc9b
m1xxos <66390094+m1xxos@users.noreply.github.com>
2026-02-17 23:17:03 +0300

    fix:s3 unified

--- terraform/1-vault/general-kv.tf
+++ terraform/1-vault/general-kv.tf
@@ -30,6 +30,30 @@ resource "vault_kv_secret_v2" "dragonfly-gl-password" {
   )
 }
 
+resource "random_string" "gitlab-s3-access-key" {
+  length  = 20
+  special = false
+  upper   = true
+  lower   = false
+  numeric = true
+}
+
+resource "random_password" "gitlab-s3-secret-key" {
+  length           = 40
+  special          = true
+  override_special = "/+"
+}
+
+resource "vault_kv_secret_v2" "gitlab-object-storage" {
+  mount = vault_mount.general.path
+  name  = "gitlab-object-storage"
+  data_json = jsonencode({
+    aws_access_key_id     = random_string.gitlab-s3-access-key.result
+    aws_secret_access_key = random_password.gitlab-s3-secret-key.result
+    bucket_region         = "us-east-1"
+  })
+}
+
 resource "vault_policy" "general-reader" {
   name   = "general-reader"
   policy = <<EOT
