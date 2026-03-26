98bae7a98915b5aae8f73d8478f89fd549410b97
m1xxos <66390094+m1xxos@users.noreply.github.com>
2026-03-26 14:27:16 +0300

    fix:rm old dns

--- terraform/0-infra/dns.tf
+++ terraform/0-infra/dns.tf
@@ -18,23 +18,6 @@ resource "cloudflare_dns_record" "main-extra" {
   ttl     = 300
 }
 
-
-resource "cloudflare_dns_record" "home" {
-  zone_id = "0f3617fcf7b220f1073d7536e1fb4aba"
-  name    = "home.m1xxos.online"
-  content = "192.168.1.128"
-  type    = "A"
-  ttl     = 300
-}
-
-resource "cloudflare_dns_record" "home-extra" {
-  zone_id = "0f3617fcf7b220f1073d7536e1fb4aba"
-  name    = "*.home.m1xxos.online"
-  content = "home.m1xxos.online"
-  type    = "CNAME"
-  ttl     = 300
-}
-
 resource "cloudflare_dns_record" "gl" {
   zone_id = local.cloudflare_zone_id
   name    = "gl.${local.domain}"
