dd0aeead83c8d1ec3359e63866f4f8ba092588f2
m1xxos <66390094+m1xxos@users.noreply.github.com>
2026-05-06 15:47:12 +0300

    chore:branch update

--- terraform/0-infra/main.tf
+++ terraform/0-infra/main.tf
@@ -36,7 +36,7 @@ module "main-cluster" {
   cluster_name         = "main"
   cluster_dns          = "main.k8s.m1xxos.online"
   github_token         = local.github_token
-  branch               = "dns-update"
+  branch               = "404-clean-up"
   cilium_version       = "1.18.6"
   clustermesh_endpoint = "192.168.1.81"
 }
