612b0da139447010ab23709417655dadcf8a65fb
m1xxos <66390094+m1xxos@users.noreply.github.com>
2026-05-18 20:42:59 +0300

    feat:custom sa for vault harbor

--- terraform/2-authentik/gh-oauth.tf
+++ terraform/2-authentik/gh-oauth.tf
@@ -19,5 +19,5 @@ resource "authentik_source_oauth" "github" {
   provider_type   = "github"
   consumer_key    = local.consumer_key
   consumer_secret = local.consumer_secret
-  oidc_jwks_url = "https://token.actions.githubusercontent.com/.well-known/jwks"
+  oidc_jwks_url   = "https://token.actions.githubusercontent.com/.well-known/jwks"
 }
