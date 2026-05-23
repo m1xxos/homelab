7f3ef0300c082a0c6095e2610a5e78cb9886db5d
m1xxos <66390094+m1xxos@users.noreply.github.com>
2026-05-23 20:50:54 +0300

    fix:reg urls

--- terraform/3-harbor/secrets.tf
+++ terraform/3-harbor/secrets.tf
@@ -1,7 +1,7 @@
 # Store robot account credentials in Vault for use by CI/CD
 resource "vault_kv_secret_v2" "harbor_robot" {
   mount = "general"
-  name  = "harbor/robot-terraform"
+  name  = "harbor/robot-k8s"
   data_json = jsonencode({
     name   = harbor_robot_account.terraform.name
     secret = harbor_robot_account.terraform.secret
@@ -13,11 +13,11 @@ resource "vault_kv_secret_v2" "harbor_proxy_registries" {
   mount = "general"
   name  = "harbor/proxy-registries"
   data_json = jsonencode({
-    docker_hub = harbor_registry.dockerhub.endpoint_url
-    quay_io    = harbor_registry.quay.endpoint_url
-    ghcr_io    = harbor_registry.ghcr.endpoint_url
-    k8s        = harbor_registry.k8s_gcr.endpoint_url
-    mcr        = harbor_registry.mcr.endpoint_url
-    gcr        = harbor_registry.gcr.endpoint_url
+    docker_hub = "${var.harbor_url}/${harbor_project.dockerhub_proxy.name}"
+    quay_io    = "${var.harbor_url}/${harbor_project.quay_proxy.name}"
+    ghcr_io    = "${var.harbor_url}/${harbor_project.ghcr_proxy.name}"
+    k8s        = "${var.harbor_url}/${harbor_project.k8s_gcr_proxy.name}"
+    mcr        = "${var.harbor_url}/${harbor_project.mcr_proxy.name}"
+    gcr        = "${var.harbor_url}/${harbor_project.gcr_proxy.name}"
   })
 }
