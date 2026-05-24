e9ae44dc0fb57aecbb209afc1c5d636ed9ff1890
m1xxos <66390094+m1xxos@users.noreply.github.com>
2026-05-24 18:03:58 +0300

    fix:enable auto sbom generation

--- terraform/3-harbor/proxy-projects.tf
+++ terraform/3-harbor/proxy-projects.tf
@@ -1,35 +1,41 @@
 resource "harbor_project" "dockerhub_proxy" {
-  name        = "dockerhub"
-  public      = true
-  registry_id = harbor_registry.dockerhub.registry_id
+  name                 = "dockerhub"
+  public               = true
+  auto_sbom_generation = true
+  registry_id          = harbor_registry.dockerhub.registry_id
 }
 
 resource "harbor_project" "ghcr_proxy" {
-  name        = "ghcr"
-  public      = true
-  registry_id = harbor_registry.ghcr.registry_id
+  name                 = "ghcr"
+  public               = true
+  auto_sbom_generation = true
+  registry_id          = harbor_registry.ghcr.registry_id
 }
 
 resource "harbor_project" "quay_proxy" {
-  name        = "quay"
-  public      = true
-  registry_id = harbor_registry.quay.registry_id
+  name                 = "quay"
+  public               = true
+  auto_sbom_generation = true
+  registry_id          = harbor_registry.quay.registry_id
 }
 
 resource "harbor_project" "k8s_gcr_proxy" {
-  name        = "k8s-gcr"
-  public      = true
-  registry_id = harbor_registry.k8s_gcr.registry_id
+  name                 = "k8s-gcr"
+  public               = true
+  auto_sbom_generation = true
+  registry_id          = harbor_registry.k8s_gcr.registry_id
 }
 
 resource "harbor_project" "mcr_proxy" {
-  name        = "mcr"
-  public      = true
-  registry_id = harbor_registry.mcr.registry_id
+  name                 = "mcr"
+  public               = true
+  auto_sbom_generation = true
+  registry_id          = harbor_registry.mcr.registry_id
 }
 
 resource "harbor_project" "gcr_proxy" {
-  name        = "gcr"
-  public      = true
-  registry_id = harbor_registry.gcr.registry_id
+  name                 = "gcr"
+  public               = true
+  auto_sbom_generation = true
+  registry_id          = harbor_registry.gcr.registry_id
 }
