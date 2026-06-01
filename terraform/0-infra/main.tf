b8ff184cbad4ab0c67db15dfa07498a4c8e91404
m1xxos <66390094+m1xxos@users.noreply.github.com>
2026-06-01 19:51:44 +0300

    chore:branch switch

--- terraform/0-infra/main.tf
+++ terraform/0-infra/main.tf
@@ -36,7 +36,7 @@ module "main-cluster" {
   cluster_name         = "main"
   cluster_dns          = "main.k8s.m1xxos.online"
   github_token         = local.github_token
-  branch               = "407-more-dashboards"
+  branch               = "409-external-cluster-logging"
   cilium_version       = "1.18.6"
   clustermesh_endpoint = "192.168.1.81"
 }
